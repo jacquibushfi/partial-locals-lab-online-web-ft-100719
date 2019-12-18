5e0801a5fa384721493111adb5d6ea8c69402553
jacquibushfi <jacqui@stainedglassofmckinney.com>
2019-12-18 12:46:21 -0600

    Done.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,10 +1,11 @@
 class ClassroomsController < ApplicationController
   def show
     @classroom = Classroom.find(params[:id])
+    @student = @classroom.student.name
     
   end
 
-  def index
-    @classrooms = Classroom.all
-  end
+  # def index
+  #   @classrooms = Classroom.all
+  # end
 end
